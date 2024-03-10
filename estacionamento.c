9bd7203f981cee0369dde2a3e667f27bde17902d
Jonathan Henrique <jonathanbispo085@gmail.com>
2024-03-10 13:30:10 -0300

    adicionado modulo de fechar caixa, relatorio com quantidade de carros estacionados em cada horario e alerta para carros ainda estacionados ao fechar do caixa.

--- estacionamento.c
+++ estacionamento.c
@@ -4,86 +4,101 @@
 #include <ctype.h>
 
 char Estaciona[30][14];
-//30 vagas
-//Mantem placa com 7 caracteres, sinal de + e 5 caracteres para hora
+int CarrosPorHorario[14];
+// 30 vagas
+// Mantem placa com 7 caracteres, sinal de + e 5 caracteres para hora
 float Valor;
 char Responsavel[21], Iniciou = 0;
 
-void AbrirCaixa(){
+void AbrirCaixa()
+{
   system("cls");
   system("color 80"); // 8 - cinza  0 - preto
   printf("\n >>> Estacionamento <<< \n");
   printf(" >>>  Largas Vagas  <<< \n");
-  printf("\n     ABRINDO  CAIXA \n");   
-  for (int i=0; i<30; i++)
-     strcpy(Estaciona[i],"LIVRE");
+  printf("\n     ABRINDO  CAIXA \n");
+  for (int i = 0; i < 30; i++)
+    strcpy(Estaciona[i], "LIVRE");
   printf("\nQual o valor do estacionamento por hora de uso? ");
-  scanf("%f",&Valor);
+  scanf("%f", &Valor);
   fflush(stdin);
   printf("Qual o nome do responsavel? ");
   gets(Responsavel);
-  Iniciou=1;
+  Iniciou = 1;
 }
 
-void ClienteChega(){
+void ClienteChega()
+{
   int Vaga;
-  char Placa[8], Hora[5], Entrada[14];
+  char Placa[8], Hora[5], Entrada[14], HoraAux[3];
   system("cls");
   system("color 80"); // 8 - cinza  0 - preto
   printf("\n >>> Estacionamento <<< \n");
   printf(" >>>  Largas Vagas  <<< \n");
-  printf("\n   CHEGADA DE CLIENTE \n");   
-  if (Iniciou){
+  printf("\n   CHEGADA DE CLIENTE \n");
+  if (Iniciou)
+  {
     printf("\n CAIXA ABERTO   Valor/h: %.2f", Valor);
     printf("\n Responsavel: %s\n\n", Responsavel);
     printf("Qual a vaga ocupada? ");
-    scanf("%d",&Vaga);
+    scanf("%d", &Vaga);
     fflush(stdin);
     printf("Qual a placa do veiculo [7 digitos]? ");
     gets(Placa);
-    strcpy(Entrada,Placa);
+    strcpy(Entrada, Placa);
     fflush(stdin);
     printf("Qual a hora de entrada [formato nn:nn]? ");
     gets(Hora);
-    strcat(Entrada,"+");
-    strcat(Entrada,Hora);
-    strcpy(Estaciona[Vaga-1],Entrada);
-    printf("Chegada registrada com sucesso!\n");}
+    strcat(Entrada, "+");
+    strcat(Entrada, Hora);
+    strcpy(Estaciona[Vaga - 1], Entrada);
+
+    HoraAux[0] = Hora[0];
+    HoraAux[1] = Hora[1];
+    HoraAux[2] = '\0';
+
+    int HoraInt = atoi(HoraAux);
+    CarrosPorHorario[HoraInt - 6] += 1;
+
+    printf("Chegada registrada com sucesso!\n");
+  }
   else
     printf("\nERRO: Antes eh preciso abrir o caixa!\n");
   system("pause");
 }
 
-void ClienteSai(){
+void ClienteSai()
+{
   int Vaga, TempoEstacionado;
   char Placa[8], Entrada[14], Hora[3], Min[3], Saida[6], HoraSaida[3], MinSaida[3];
-  int H, M; //hora, minuto e segundo DE ENTRADA
-  int Hs, Ms; //hora, minuto e segundo DE SAIDA
+  int H, M;   // hora, minuto e segundo DE ENTRADA
+  int Hs, Ms; // hora, minuto e segundo DE SAIDA
   system("cls");
   system("color 80"); // 8 - cinza  0 - preto
   printf("\n >>> Estacionamento <<< \n");
   printf(" >>>  Largas Vagas  <<< \n");
-  printf("\n    SAIDA DE CLIENTE \n");   
-  if (Iniciou){
+  printf("\n    SAIDA DE CLIENTE \n");
+  if (Iniciou)
+  {
     printf("\n CAIXA ABERTO   Valor/h: %.2f", Valor);
     printf("\n Responsavel: %s\n\n", Responsavel);
     printf("Qual a vaga ocupada? ");
-    scanf("%d",&Vaga);
+    scanf("%d", &Vaga);
     fflush(stdin);
-    
-    //se vaga LIVRE, houve erro
-    
-    strcpy(Entrada,Estaciona[Vaga-1]);
-    
-    //Teste
-    printf("\n>>>>>>>%s",Entrada);
-   
-    for (int i=0; i<7; i++)
+
+    // se vaga LIVRE, houve erro
+
+    strcpy(Entrada, Estaciona[Vaga - 1]);
+
+    // Teste
+    printf("\n>>>>>>>%s", Entrada);
+
+    for (int i = 0; i < 7; i++)
       Placa[i] = toupper(Entrada[i]);
-     
-     //Teste
-     printf("\n>>>>>>>Placa: %s",Placa);
-    
+
+    // Teste
+    printf("\n>>>>>>>Placa: %s", Placa);
+
     Hora[0] = Entrada[8];
     Hora[1] = Entrada[9];
     Hora[2] = '\0';
@@ -92,12 +107,12 @@ void ClienteSai(){
     Min[1] = Entrada[12];
     Min[2] = '\0';
     M = atoi(Min); // convete string em inteiro
-    
-    //Teste
-    printf("\n>>>>>>>Hora: %d",H);
-    printf("\n>>>>>>>Minutos: %d\n",M);
-    
-	printf("Qual eh o horario de saida [formato nn:nn]? ");
+
+    // Teste
+    printf("\n>>>>>>>Hora: %d", H);
+    printf("\n>>>>>>>Minutos: %d\n", M);
+
+    printf("Qual eh o horario de saida [formato nn:nn]? ");
     gets(Saida);
     fflush(stdin);
     HoraSaida[0] = Saida[0];
@@ -111,38 +126,59 @@ void ClienteSai(){
 
     TempoEstacionado = (Hs - H) * 60 + (Ms - M);
     float ValorRequisitado, QuantiaPaga, Troco;
-    ValorRequisitado = (float)TempoEstacionado * Valor/60;
+    ValorRequisitado = (float)TempoEstacionado * Valor / 60;
     printf("\n======================\n");
-    do{
-        printf("Valor a pagar: R$%.2f\n", ValorRequisitado);
-        printf("Qual foi a quantia de pagamento? ");
-        scanf("%f", &QuantiaPaga);
-        fflush(stdin);
-        if(QuantiaPaga>=ValorRequisitado){
-            Troco = QuantiaPaga - ValorRequisitado;
-            printf("\nValor pago: R$%.2f.\n", QuantiaPaga);
-            printf("O troco eh de R$%.2f.\n", Troco);
-        }
-        else{
-            system("cls");
-            printf("\nQuantia insuficiente para pagamento!\n");
-        }
-    } while(QuantiaPaga<ValorRequisitado);
+    do
+    {
+      printf("Valor a pagar: R$%.2f\n", ValorRequisitado);
+      printf("Qual foi a quantia de pagamento? ");
+      scanf("%f", &QuantiaPaga);
+      fflush(stdin);
+      if (QuantiaPaga >= ValorRequisitado)
+      {
+        Troco = QuantiaPaga - ValorRequisitado;
+        printf("\nValor pago: R$%.2f.\n", QuantiaPaga);
+        printf("O troco eh de R$%.2f.", Troco);
+      }
+      else
+      {
+        system("cls");
+        printf("\nQuantia insuficiente para pagamento!\n");
+      }
+    } while (QuantiaPaga < ValorRequisitado);
+    strcpy(Estaciona[Vaga-1], "LIVRE");
     printf("\n======================\n");
     printf("Operacao Realizada Com Sucesso!\n");
-
-	  }
- else
+  }
+  else
     printf("\nERRO: Antes eh preciso abrir o caixa!\n");
   system("pause");
 }
 
 void FecharCaixa(){
+  system("cls");
+  system("color 80"); // 8 - cinza  0 - preto
+  printf("\n >>> Estacionamento <<< \n");
+  printf(" >>>  Largas Vagas  <<< \n");
+  printf("\n    FECHANDO CAIXA \n");
+  printf("<============= Relatorio =============>\n");
+  for(int i = 0; i<14; i++){
+    printf("Horario: %d horas | Quantidade de carros: %d\n", i+6, CarrosPorHorario[i]);
+  }
+
+  printf("<=====================================>\n\n");
+  for(int i = 0; i<30; i++){
+    if(strcmp(Estaciona[i], "LIVRE")!= 0){
+      printf("ALERTA: Ainda ha um carro estacionado na vaga %d.\n", i+1);
+    }
+  }
 }
 
-int main(){
+int main()
+{
   int Op;
-  do{
+  do
+  {
     system("cls");
     system("color 70"); // 7 - branco  0 - preto
     printf("\n >>> Estacionamento <<< \n");
@@ -152,23 +188,33 @@ int main(){
     printf("\n 3 - Saida de Cliente");
     printf("\n 4 - Fechar Caixa");
     printf("\n\n Qual a opcao desejada? ");
-    do{
-      scanf("%d",&Op);
-      if ((Op < 1) || (Op > 4)){
+    do
+    {
+      scanf("%d", &Op);
+      if ((Op < 1) || (Op > 4))
+      {
         printf("\n ERRO: Opcao invalida!");
-        printf("\n Qual a opcao desejada? ");}
+        printf("\n Qual a opcao desejada? ");
+      }
       else
         break;
-    }while (1);
-    switch (Op) {
-	  case 1: AbrirCaixa();
-	          break;
-	  case 2: ClienteChega();
-	          break;
-	  case 3: ClienteSai();
-	          break;
-	  case 4: FecharCaixa();
-	          break;}
-  }while (Op != 4);
-	
-return 0;}
+    } while (1);
+    switch (Op)
+    {
+    case 1:
+      AbrirCaixa();
+      break;
+    case 2:
+      ClienteChega();
+      break;
+    case 3:
+      ClienteSai();
+      break;
+    case 4:
+      FecharCaixa();
+      break;
+    }
+  } while (Op != 4);
+
+  return 0;
+}
